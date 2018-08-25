f9eb8005e6d80c6f87bb7e8d929e4d41660bc33c
Shepherd <shepherd_dc@163.com>
2018-08-25 17:07:53 +0800

    jsonify replace json.dumps

--- fisher.py
+++ fisher.py
@@ -1,4 +1,4 @@
-from flask import Flask, json
+from flask import Flask, jsonify
 
 from book import Book
 from helper import is_isbn_or_key
@@ -15,14 +15,11 @@ def hello():
 @app.route('/book/search/<q>/<page>')
 def search(q, page):
     isbn_or_key = is_isbn_or_key(q)
-    headers = {
-        'content-type': 'application/json'
-    }
     if isbn_or_key == 'isbn':
         result = Book.search_by_isbn(q)
     else:
         result = Book.search_by_keyword(q)
-    return json.dumps(result), 200, headers
+    return jsonify(result)
 
 
 if __name__ == '__main__':
