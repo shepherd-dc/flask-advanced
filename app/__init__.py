56bc91b5a710d2a9e8aead910842dc046aa57f42
Shepherd <shepherd_dc@163.com>
2018-08-27 23:34:13 +0800

    SQLAlchemy Model book

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+from app.models.book import db
 from app.web import web
 
 
@@ -8,6 +9,9 @@ def create_app():
     app.config.from_object('app.secure')
     app.config.from_object('app.setting')
     register_blueprint(app)
+
+    db.init_app(app)
+    db.create_all(app=app)
     return app
 
 
--- app/secure.py
+++ app/secure.py
@@ -1,1 +1,14 @@
 DEBUG = True
+
+# dialect+driver://username:password@host:port/database
+DIALECT = 'mysql'
+DRIVER = 'cymysql'
+USERNAME = 'root'
+PASSWORD = 123456
+HOST = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'fisher'
+
+SQLALCHEMY_DATABASE_URI = '{}+{}://{}:{}@{}:{}/{}?charset=utf8'\
+    .format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
