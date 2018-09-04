f35a4051cb8e275d540edff817ecf4624792c382
shepherd_dc <shepherd_dc@aliyun.com>
2018-09-04 14:03:39 +0800

    homepage recent gifts

--- app/models/gift.py
+++ app/models/gift.py
@@ -1,7 +1,9 @@
-from sqlalchemy import Column, Integer, Boolean, ForeignKey, String
+from flask import current_app
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, desc
 from sqlalchemy.orm import relationship
 
 from app.models.base import Base
+from app.spider.book import Book
 
 
 class Gift(Base):
@@ -12,3 +14,15 @@ class Gift(Base):
     # book = relationship('Book')
     # bid = Column(Integer, ForeignKey('book.id'))
     isbn = Column(String(15), nullable=False)
+
+    @classmethod
+    def recent(cls):
+        recent_gifts = Gift.query.filter_by(launched=False).group_by(Gift.isbn).order_by(desc(Gift.create_time)).limit(
+            current_app.config['RECENT_BOOKS_COUNT']).distinct().all()
+        return recent_gifts
+
+    @property
+    def book(self):
+        book = Book()
+        book.search_by_isbn(self.isbn)
+        return book.first
