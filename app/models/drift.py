0e027afafd9b3590aca513efe28fd860e080feb5
Shepherd <shepherd_dc@163.com>
2018-09-06 22:42:53 +0800

    drift pending

--- app/models/drift.py
+++ app/models/drift.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, SmallInteger
 
+from app.libs.enums import PendingStatus
 from app.models.base import Base
 
 
@@ -30,4 +31,12 @@ class Drift(Base):
     gift_id = Column(Integer)
     gifter_nickname = Column(String(20))
 
-    pending = Column('pending', SmallInteger, default=1)
+    _pending = Column('pending', SmallInteger, default=1)
+
+    @property
+    def pending(self):
+        return PendingStatus(self._pending)
+
+    @pending.setter
+    def pending(self, status):
+        self._pending = status.value
--- app/web/drift.py
+++ app/web/drift.py
@@ -4,9 +4,12 @@ from sqlalchemy import or_, desc
 
 from app.forms.book import DriftForm
 from app.libs.email import send_mail
+from app.libs.enums import PendingStatus
 from app.models.base import db
 from app.models.drift import Drift
 from app.models.gift import Gift
+from app.models.user import User
+from app.models.wish import Wish
 from app.view_models.drift import DriftCollection
 from . import web
 
@@ -34,28 +37,51 @@ def send_drift(gid):
     gifter = current_gift.user.summary
     return render_template('drift.html', gifter=gifter, user_beans=current_user.beans, form=form)
 
+
 @web.route('/pending')
 @login_required
 def pending():
-    drifts = Drift.query.filter(or_(Drift.requester_id==current_user.id, Drift.gifter_id==current_user.id)
+    drifts = Drift.query.filter(or_(Drift.requester_id == current_user.id, Drift.gifter_id == current_user.id)
                                 ).order_by(desc(Drift.create_time)).all()
     views = DriftCollection(drifts, current_user.id)
     return render_template('pending.html', drifts=views.data)
 
 
 @web.route('/drift/<int:did>/reject')
+@login_required
 def reject_drift(did):
-    pass
+    with db.auto_commit():
+        drift = Drift.query.filter(Drift.id == did, Gift.uid == current_user.id).first_or_404()
+        drift.pending = PendingStatus.Reject
+        requester = User.query.get_or_404(drift.requester_id)
+        requester.beans += 1
+    return redirect(url_for('web.pending'))
 
 
 @web.route('/drift/<int:did>/redraw')
+@login_required
 def redraw_drift(did):
-    pass
+    with db.auto_commit():
+        drift = Drift.query.filter_by(id=did, requester_id=current_user.id).first_or_404()
+        drift.pending = PendingStatus.Redraw
+        current_user.beans += 1
+    return redirect(url_for('web.pending'))
 
 
 @web.route('/drift/<int:did>/mailed')
+@login_required
 def mailed_drift(did):
-    pass
+    with db.auto_commit():
+        drift = Drift.query.filter_by(id=did, gifter_id=current_user.id).first_or_404()
+        drift.pending = PendingStatus.Success
+        current_user.beans += 1
+
+        gift = Gift.query.filter_by(id=drift.gift_id).first_or_404()
+        gift.launched = True
+
+        Wish.query.filter_by(isbn=drift.isbn, uid=drift.requester_id, launched=False).update({Wish.launched: True})
+
+    return redirect(url_for('web.pending'))
 
 
 def save_drift(drift_form, current_gift):
